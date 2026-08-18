98b6e04e347d26d39d3f6af05c02c063740b4976
nindya <nindya@nindyas-MacBook-Air.local>
2026-08-18 13:37:47 +0700

    delete movie studio 4

--- app/main.py
+++ app/main.py
@@ -9,7 +9,6 @@ MOVIES = [
     {"title": "The Odyssey", "showtime": "13:00", "studio": "Studio 1"},
     {"title": "Spider-Man: Brand New Day", "showtime": "15:00", "studio": "Studio 2"},
     {"title": "The Matrix Resurrections", "showtime": "17:00", "studio": "Studio 3"},
-    {"title": "Dune", "showtime": "19:00", "studio": "Studio 4"},
 ]
 
 @app.route("/")
